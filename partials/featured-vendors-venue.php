0d96ceec6eda764eb9f29ba16166664e257e3031
Andrew Ip <andrewip@andrewip.co.mentel.com>
2013-11-08 11:22:46 -0500

    Fix adrotate for local-venues i.e. http://www.occasionsonline.com/local/florida/south-florida/venues/hotels

--- partials/featured-vendors-venue.php
+++ partials/featured-vendors-venue.php
@@ -4,5 +4,8 @@
 ?>
 <section class="featured-vendors featured-vendors-large">
 	<h4>Featured Vendors</h4>
-	<?php echo flo_adrotate_block('venue', $type->term_id, 0, 4); ?>
+	<?php 
+			$region = get_term_by('slug', $region, 'region');
+	 ?>
+	<?php echo flo_adrotate_block('local-venue', $region->term_id, $type->term_id, 4); ?>
 </section>
